4d94e619e5ddd153e4f573153e3dab918b734e17
maksym hromyk <maksym_hromyk@epam.com>
2018-09-04 16:58:30 +0200

    implementing Percolation and two additional classes

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,21 +1,21 @@
 /**
  * Created by Maks on 8/31/2018.
  */
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int numberOfOpenSites;
     private WeightedQuickUnionUF graph;
     private int sizeOfTheGrid;
+    //        initialize array for tracking state of the sites.
+    private Site sites;
 
     // virtual node for simplifying checking percolation by joining all opened nodes from top row with virtual top node and as a result we will check percolation staring from virtual top node.
     private int virtualTop;
     private int virtualDown;
 
     private int getIndex(int row, int col){
-        if (row<=0) throw IllegalArgumentException("element is outside of ")
+        if ( (row<=0) && (col<=0)) throw new IllegalArgumentException("specified ("+row+","+col+") indices are outside of boundaries.");
         return (row-1)*sizeOfTheGrid+(col-1);
     }
 
@@ -25,31 +25,36 @@ public class Percolation {
         graph = new WeightedQuickUnionUF(n+2);
         virtualTop = n;
         virtualDown = n+1;
+
+        sites = new Site(n+2);
+        // open VirtualTop and VirtualBottom sites to be ready for connection
+        sites.openSite(virtualTop);
+        sites.openSite(virtualDown);
     }
 
-    // open site (row, col) if it is not open already
-    public    void open(int row, int col){
-
+    // open sites (row, col) if it is not open already
+    public  void open(int row, int col){
+        sites.openSite(getIndex(row,col));
     }
 
-    // is site (row, col) open?
+    // is sites (row, col) open?
     public boolean isOpen(int row, int col){
-
+        return sites.getState(getIndex(row,col))==State.OPENED;
     }
 
-    // is site (row, col) full?
+    // is sites (row, col) full?
     public boolean isFull(int row, int col){
-
+        return graph.connected(virtualTop,getIndex(row,col));
     }
 
     // number of open sites
-    public     int numberOfOpenSites(){
-
+    public int numberOfOpenSites(){
+        return sites.getOpenedSites();
     }
 
     // does the system percolate?
     public boolean percolates(){
-
+        return graph.connected(virtualTop,virtualDown);
     }
 
     // test client (optional)
---
+++ src/main/java/Site.java
@@ -0,0 +1,46 @@
+/**
+ * The {@code Site} class represents interface for
+ * tracking the state of the <b>{@code n}</b> array of sites.
+ * Site can have one of the following states:
+ *  <li> {@code 0} - is closed site
+ *  <li> {@code 1} - is opened site
+ *  <li> {@code 2} - is full site
+ *
+ *
+ * */
+public class Site {
+    private State[] site;
+    private final int size;
+    private int openedSites;
+
+    public Site(int size) {
+        this.site = new State[size];
+        this.size = size;
+        for (int i=0;i<size;i++){
+            site[i] = State.CLOSED;
+        }
+    }
+
+    public State getState(int i){
+        return site[i];
+    }
+
+    public void openSite(int i){
+        if(site[i]==State.CLOSED){
+            openedSites++;
+            site[i]=State.OPENED;
+        }
+    }
+
+    public void makeFullSite(int i){
+        site[i]=State.FULL;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getOpenedSites(){
+        return openedSites;
+    }
+}
